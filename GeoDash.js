7822c6cb944eac013eb6cfd4f982ada6fd68949d
SpaceKat154 <161382600+SpaceKat154@users.noreply.github.com>
2024-02-27 10:51:00 +1300

    Update GeoDash.js
    no more double jumping poggers

--- GeoDash.js
+++ GeoDash.js
@@ -1,7 +1,7 @@
 /*******************************************************/
-// P5.play: t22_keyboard
-// Move sprite based on keyboard input
-// Written by ???
+// P5.play: GeoDash Demo Game
+// Demo Game
+// Written by Byron Thistoll
 /*******************************************************/
 console.log("%c t22_keyboard.js", "color: blue;");
 
@@ -15,16 +15,18 @@ const SCREENWIDTH = 1920;
 function setup() {
   console.log("setup: ");
   cnv = new Canvas(SCREENWIDTH, SCREENHEIGHT);
-  world.gravity.y = 10;
+  world.gravity.y = 274;
   //boxBox
   boxBoi = new Sprite(100, 350, 50, 50, 'd');
   boxBoi.color = 'yellow';
   boxBoi.bounciness = 0;
    document.addEventListener("keydown", function(event) {
       if (event.code === 'ArrowUp') {
+          if (boxBoi.vel.y == 0){
           // Set sprite's velocity upwards
-          boxBoi.vel.y = -5;
+          boxBoi.vel.y = -50;
           console.log("go up");
+          }
       }
   });
   
