78e6a08a2bc65c35c7eb0ad73881d08c3e3ced2e
SpaceKat154 <161382600+SpaceKat154@users.noreply.github.com>
2024-03-04 09:54:43 +1300

    Update GeoDash.js
    more obstacles and screens

--- GeoDash.js
+++ GeoDash.js
@@ -8,7 +8,15 @@ console.log("%c t22_keyboard.js", "color: blue;");
 //variables
 const SCREENHEIGHT = 920;
 const SCREENWIDTH = 1920;
+const PLAYER_HEIGHT = 25;
+const PLAYER_WIDTH = 25;
+const OBSTACLE_HEIGHT = PLAYER_HEIGHT;
+const OBSTACLE_WIDTH = PLAYER_WIDTH;
 var status = 'start';
+var score = 0;
+var obstacles;
+var nextSpawn = 0;
+var  spawnDist = 0+1;
 
 /*******************************************************/
 // setup()
@@ -23,11 +31,14 @@ function setup() {
   boxBoi.color = 'yellow';
   boxBoi.bounciness = 0;
    document.addEventListener("keydown", function(event) {
-      if (event.code === 'ArrowUp') {
+       if (status == 'start'){
+           status = 'game';
+       }
+       if (event.code === 'ArrowUp') {
           if (boxBoi.vel.y == 0){
-          // Set sprite's velocity upwards
-          boxBoi.vel.y = -50;
-          console.log("go up");
+              // Set sprite's velocity upwards
+              boxBoi.vel.y = -50;
+              console.log("go up");
           }
       }
   });
@@ -37,17 +48,8 @@ function setup() {
   wallBot.color = 'green';
   wallBot.bounciness = 0;
   
-  //obstacles
-  spike = new Sprite(SCREENWIDTH - 100, 350, 20, 30, 'd');
-  spike.color = 'black';
-  spike.bounciness = 0;
-  spike.friction = 0;
-  spike.vel.x = -10;
-  if (spike.x == 10){
-      spike.x = SCREENWIDTH - 100;
-  }
-  
-  boxBoi.collides(spike, deathScreen());
+  obstacles = new Group();
+  boxBoi.collides(obstacles, deathScreen());
 }
 
 /*******************************************************/
@@ -63,24 +65,47 @@ function draw() {
   }
 }
 
+function newObstacle(){
+    spike = new Sprite((SCREENWIDTH -100),  SCREENHEIGHT - OBSTACLE_HEIGHT/2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
+    spike.color = 'black';
+    spike.bounciness = 0;
+    spike.friction = 0;
+    spike.vel.x = -10;
+    obstacles.add(spike);
+}
+
 function startScreen(){
     background('gray');
-    status = 'game';
+    allSprites.visible = false;
+    textSize(32);
+    fill(255);
+    stroke(0);
+    strokeWeight(4);
+    text("Welcome to GeoRunner", 50, 50);
+    textSize(24);
+    text("Press any key to start", 50, 110);
 }
 
 function gameScreen(){
     background('blue');
+    score++;
+    if(frameCount> nextSpawn){
+        newObstacle();
+        nextSpawn = frameCount + random(10,100);
+    }
+    allSprites.visible = true;
     if (spike.x == 10){
       spike.x = SCREENWIDTH - 100;
     }
     if (spike.collides(boxBoi) == true) {
+        console.log('you died');
         status = 'death';
     }
 }
 
 function deathScreen() {
-    console.log('you died');
     background("red");
+    allSprites.visible = false;
 }
 /*******************************************************/
 //  END OF APP
